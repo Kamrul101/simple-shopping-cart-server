7edd2f829b6945a3c4c01aff133c554ab679537f
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2023-09-05 20:15:28 +0600

    delete

--- index.js
+++ index.js
@@ -31,7 +31,27 @@ async function run() {
         const result = await shoeCollection.find().toArray();
         res.send(result);
     })
-    
+
+    app.get("/cart", async(req,res)=>{
+        const email = req.query.email;
+        const query = {email: email};
+        const result = await cartCollection.find(query).toArray();
+        res.send(result);
+    })
+
+
+    app.post('/cart',async(req,res)=>{
+        const item = req.body;
+        const result = await cartCollection.insertOne(item);
+        res.send(result)
+    })
+
+    app.delete('/cart/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await cartCollection.deleteOne(query);
+        res.send(result)
+      })
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
